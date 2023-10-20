1b8cf7c18c05d5798e04790547d917ad74d06474
Admin <admin@example.com>
2023-10-20 11:35:45 +0100

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingComponent } from './listing/listing.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
