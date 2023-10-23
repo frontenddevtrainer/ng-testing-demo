3a9422aa96ec5495afbe7ff562a331697f55cb73
Admin <admin@example.com>
2023-10-23 13:23:15 +0100

    Test get request.

--- src/app/people.service.spec.ts
+++ src/app/people.service.spec.ts
@@ -1,19 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PeopleService } from './people.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { IUser } from './interfaces/user';
 
 describe('PeopleService', () => {
   let service: PeopleService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(PeopleService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch data using HTTP', () => {
+    const MOCK_DATA: Partial<IUser>[] = [
+      {
+        id: 1,
+        username: 'abc',
+      },
+    ];
+
+    service.getPeopleDataWithAPI().subscribe((response) => {
+      expect(response.length).toBe(1);
+    });
+
+    const req = httpMock.expectOne("https://jsonplaceholder.typicode.com/users");
+    expect(req.request.method).toBe("GET");
+    req.flush(MOCK_DATA);
+
+  });
+
+  afterEach(()=>{
+    httpMock.verify()
+  })
 });
